c342b4425b74c01e36b1b53e71fa518dc1313300
Daniel Ebuka Mabia <danielebukamabia@gmail.com>
2024-03-20 19:19:37 +0000

    task 2

---
+++ 0x00-MySQL_Advanced/2-fans.sql
@@ -0,0 +1,8 @@
+-- SQL script that ranks country origins of bands, ordered by the number of (non-unique) fans
+
+-- Querying a table that contains data on band music
+
+SELECT origin, SUM(fans) AS nb_fans
+FROM metal_bands
+GROUP BY origin
+ORDER BY nb_fans DESC;
