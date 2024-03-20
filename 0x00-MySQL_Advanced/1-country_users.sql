5d371a7fe6d95fc475387dbbfea40646430b4032
Daniel Ebuka Mabia <danielebukamabia@gmail.com>
2024-03-20 15:20:56 +0000

    task 1

---
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -0,0 +1,19 @@
+-- An SQL script that creates a new table called users
+
+-- Creating a new table based on the following requirements
+-- id, integer, never null, auto increment and primary key
+-- email, string (255 characters), never null and unique
+-- name, string (255 characters)
+-- If the table already exists, your script should not fail
+-- country, enumeration of countries: US, CO and TN
+
+-- Drop the table if it already exist and create a new one
+
+DROP TABLE IF EXISTS users;
+
+CREATE TABLE IF NOT EXISTS users (
+	id INT PRIMARY KEY NOT NULL UNIQUE AUTO_INCREMENT,
+	email VARCHAR(255) NOT NULL UNIQUE,
+	name VARCHAR(255),
+	country ENUM('US', 'CO', 'TN') DEFAULT 'US'
+	);
