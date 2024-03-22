81091ddf76aff17b621f556e27745d47ef99daa1
Daniel Ebuka Mabia <danielebukamabia@gmail.com>
2024-03-22 18:35:16 +0000

    removed the else statement as it was not necessary

--- 0x00-MySQL_Advanced/5-valid_email.sql
+++ 0x00-MySQL_Advanced/5-valid_email.sql
@@ -8,7 +8,6 @@ CREATE TRIGGER validate_email BEFORE UPDATE
 FOR EACH ROW
 	BEGIN
 		IF NEW.name != OLD.name THEN SET NEW.valid_email = 0;
-		ELSE SET NEW.valid_email = 1;
 		END IF;
 	END; //
 -- Revert DELIMITER
