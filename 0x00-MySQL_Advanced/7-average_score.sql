4f75286f36d14c8e3c171a622122cdd69c418de0
Daniel Ebuka Mabia <danielebukamabia@gmail.com>
2024-03-22 12:30:00 +0000

    task 7

--- 0x00-MySQL_Advanced/7-average_score.sql
+++ 0x00-MySQL_Advanced/7-average_score.sql
@@ -8,17 +8,15 @@ DELIMITER // ;
 
 CREATE PROCEDURE ComputeAverageScoreForUser(user_id INT)
 BEGIN
-	DECLARE average FLOAT;
 	DECLARE total_score INT;
 	DECLARE total_project INT;
 	-- Obtain the total score of a particular student.
 	SET total_score = (SELECT SUM(score) FROM corrections WHERE user_id = user_id);
 	-- Obtain the total number of project of a particular student.
 	SET total_project =(SELECT DISTINCT COUNT(project_id) FROM corrections WHERE user_id = user_id);
-	SET average = IF(total_project > 0, total_score / total_project, 0);
 	UPDATE users
-	SET average_score = average
-	WHERE id = user_id;
+	SET average_score = IF(total_project > 0, total_score / total_project, 0)
+	WHERE users.id = user_id;
 END; //
 
 -- Revert DELIMITER
