54856dc6e35ef6c11aeebebc7730c68d793cd258
Daniel Ebuka Mabia <danielebukamabia@gmail.com>
2024-03-22 18:22:02 +0000

    Task 5

---
+++ 0x00-MySQL_Advanced/5-valid_email.sql
@@ -0,0 +1,16 @@
+-- SQL script that creates a trigger that resets the attribute valid_email only when the email has been changed.
+
+-- Briefly change DELIMITER
+
+DELIMITER // ;
+
+CREATE TRIGGER validate_email BEFORE UPDATE
+FOR EACH ROW
+	BEGIN
+		IF NEW.name != OLD.name THEN SET NEW.valid_email = 0
+		ELSE SET NEW.valid_email = 1
+		END IF;
+	END; //
+-- Revert DELIMITER
+
+DELIMITER ; //
