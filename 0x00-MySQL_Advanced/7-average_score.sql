787b714acf383d1ec43cf2624c342445728c2119
Daniel Ebuka Mabia <danielebukamabia@gmail.com>
2024-03-23 17:09:16 +0000

    task 7 added table name to where clause to avoid ambiguity

--- 0x00-MySQL_Advanced/7-average_score.sql
+++ 0x00-MySQL_Advanced/7-average_score.sql
@@ -11,12 +11,12 @@ BEGIN
 	DECLARE total_score INT;
 	DECLARE total_project INT;
 	-- Obtain the total score of a particular student.
-	SET total_score = (SELECT SUM(score) FROM corrections WHERE user_id = user_id);
+	SET total_score = (SELECT SUM(score) FROM corrections WHERE corrections.user_id = user_id);
 	-- Obtain the total number of project of a particular student.
-	SET total_project =(SELECT COUNT(DISTINCT project_id) FROM corrections WHERE user_id = user_id);
+	SET total_project =(SELECT COUNT(DISTINCT project_id) FROM corrections WHERE corrections.user_id = user_id);
 	UPDATE users
 	SET average_score = IF(total_project > 0, total_score / total_project, 0)
-	WHERE id = user_id;
+	WHERE users.id = user_id;
 END; //
 
 -- Revert DELIMITER
