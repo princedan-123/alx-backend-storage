1360f85806f5d1f5761cfcd0b33eff7a942514bc
Daniel Ebuka Mabia <danielebukamabia@gmail.com>
2024-03-26 10:03:49 +0000

    Task 9

---
+++ 0x01-NoSQL/9-insert_school.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""A python script that inserts a document into a collection in MongoDB."""
+
+
+def insert_school(mongo_collection, **kwargs):
+    """
+        A function that adds a document to collection
+        Args: mongo_collection (collection object)
+              kwargs(a dictionary)
+        Return: the insertion id of the document if successful
+    """
+    result = mongo_collection.insert_one(kwargs)
+    return result.get(inserted_id, '')
