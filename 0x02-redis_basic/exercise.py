0a1f51a980289567183ec3fbee1dc69334990102
Daniel Ebuka Mabia <danielebukamabia@gmail.com>
2024-03-27 14:50:26 +0000

    task 1

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 """A python script that creates a cache using redis."""
-import typing
+from typing import Union
+from typing import Callable, Any
 import redis
 import uuid
 
@@ -12,8 +13,24 @@ class Cache:
         self._redis = redis.Redis()
         self._redis.flushdb()
 
-    def store(self, data: typing.Union[str, bytes, int, float]) -> str:
+    def store(self, data: Union[str, bytes, int, float]) -> str:
         """An instance method that stores data to redis instance."""
         key = str(uuid.uuid4())
         self._redis.set(key, data)
         return key
+
+    def get(self, key: str, fn: Any = None) -> Union[bytes, Any]:
+        """An instance method that retreives data from redis."""
+        if fn is not None:
+            data = self._redis.get(key)
+            if data is not None:
+                return fn(data)
+        return self._redis.get(key)
+
+    def get_str(self, key: str) -> str:
+        """An instance method that converts retrieved data to string."""
+        return self.get(key).decode('utf-8')
+
+    def get_int(self, key: str) -> int:
+        """An instance method that converts a retrieved data to integer."""
+        return int.from_bytes(self.get(key), byteorder='big')
