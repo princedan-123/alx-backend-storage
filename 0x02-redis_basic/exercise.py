020fab3ce6bdc0b912cc17c2dafdfbff1a68b14b
Daniel Ebuka Mabia <danielebukamabia@gmail.com>
2024-03-27 12:51:11 +0000

    task 0

---
+++ 0x02-redis_basic/exercise.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""A python script that creates a cache using redis."""
+import typing
+import redis
+import uuid
+
+
+class Cache:
+    """A class that uses the Redis Class to create a cache."""
+    def __init__(self):
+        """Initialization method of the Cache class."""
+        self._redis = redis.Redis()
+        self._redis.flushdb()
+
+    def store(self, data: typing.Union[str, int, float]) -> uuid.UUID:
+        """An instance method that stores data to redis instance."""
+        key = uuid.uuid4()
+        self._redis.set(key, data)
+        return key
