7adfd32b5e5022fd8a8f9b8e38d9710ea30d1fd9
Daniel Ebuka Mabia <danielebukamabia@gmail.com>
2024-03-27 12:56:42 +0000

    converted uuid to string

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -12,7 +12,7 @@ class Cache:
         self._redis = redis.Redis()
         self._redis.flushdb()
 
-    def store(self, data: typing.Union[str, int, float]) -> str:
+    def store(self, data: typing.Union[str, bytes, int, float]) -> str:
         """An instance method that stores data to redis instance."""
         key = str(uuid.uuid4())
         self._redis.set(key, data)
