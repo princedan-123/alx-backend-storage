ae1ae69e91781c59b0dc4bb3f496b88232aedb55
Daniel Ebuka Mabia <danielebukamabia@gmail.com>
2024-03-27 18:49:34 +0000

    task 3

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -19,6 +19,20 @@ def count_calls(method: Callable) -> Callable:
     return wrapper
 
 
+def call_history(method: Callable) -> Callable:
+    """A decorator that stores its input and output in redis."""
+    @wraps(method)
+    def wrapper(self, data):
+        """A wrapper function that wraps the original function."""
+        key_input = f"{method.__qualname__}:inputs"
+        key_output = f"{method.__qualname__}:outputs"
+        self._redis.rpush(key_input, str(data))
+        result = method(self, data)
+        self._redis.rpush(key_output, result)
+        return result
+    return wrapper
+
+
 class Cache:
     """A class that uses the Redis Class to create a cache."""
     def __init__(self):
@@ -26,6 +40,7 @@ class Cache:
         self._redis = redis.Redis()
         self._redis.flushdb()
 
+    @call_history
     @count_calls
     def store(self, data: Union[str, bytes, int, float]) -> str:
         """An instance method that stores data to redis instance."""
